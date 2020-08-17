65317aa60a2db772d735bafeaf9c3b65049785be
octavialarentisgsa <52218712+octavialarentisgsa@users.noreply.github.com>
2020-08-17 14:41:56 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,34 @@
+variable "aws_key_name" {
+  type = string
+  description = "aws ec2 key name used to authenticate"
+}
+
+variable "subnets_private_id" {
+  type    = list(string)
+  description = "List of Subnet ID for EC2 instances - must cross 2 AZs due to EKS requirements"
+}
+
+variable "vpc_id" {
+  type = string
+  description = "ID of the VPC "
+}
+
+variable "subnet_id" {
+  type = string
+  description = "ID of instance subnet "
+}
+
+variable "cidr_block_worker_group_1" {
+  type    = list(string)
+  description = "List of CIDR blocks needing access to Worker Group 1"
+}
+
+variable "cidr_block_worker_group_2" {
+  type    = list(string)
+  description = "List of CIDR blocks needing access to Worker Group 2"
+}
+
+variable "cidr_block_all_workers" {
+  type    = list(string)
+  description = "List of CIDR blocks needing access to all Worker Groups"
+}
