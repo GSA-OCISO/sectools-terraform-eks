a36cc58e8b9e4c92a25071f65540ca5588bc3765
octavialarentisgsa <52218712+octavialarentisgsa@users.noreply.github.com>
2020-08-17 14:48:08 -0400

    Create eks-cluster.tf

---
+++ eks-cluster.tf
@@ -0,0 +1,42 @@
+module "eks" {
+  source       = "terraform-aws-modules/eks/aws"
+  cluster_name = local.cluster_name
+  subnets = var.subnets_private_id
+
+  tags = {
+    Environment = "training"
+    GithubRepo  = "terraform-aws-eks"
+    GithubOrg   = "terraform-aws-modules"
+  }
+
+  vpc_id = var.vpc_id
+
+  worker_groups = [
+    {
+      name                          = "worker-group-1"
+      instance_type                 = "t2.small"
+      additional_userdata           = "echo foo bar"
+      asg_desired_capacity          = 3
+      key_name                      = var.aws_key_name
+      subnet_id                     = var.subnet_id
+      additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
+    },
+    {
+      name                          = "worker-group-2"
+      instance_type                 = "t2.medium"
+      additional_userdata           = "echo foo bar"
+      additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
+      asg_desired_capacity          = 3
+      key_name                      = var.aws_key_name
+      subnet_id                     = var.subnet_id
+    },
+  ]
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
