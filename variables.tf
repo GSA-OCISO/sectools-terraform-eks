2b46b266daa5dd59c6bce05d4e49193f55101efe
octavialarentisgsa <52218712+octavialarentisgsa@users.noreply.github.com>
2020-08-17 16:34:28 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,6 +3,11 @@ variable "aws_key_name" {
   description = "aws ec2 key name used to authenticate"
 }
 
+variable "instance_type" {
+  description = "EC2 instance type"
+  type = string
+}
+
 variable "subnets_private_id" {
   type    = list(string)
   description = "List of Subnet ID for EC2 instances - must cross 2 AZs due to EKS requirements"
