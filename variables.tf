707c123c05d97260e060c632e6c7b1a46d3960f0
octavialarentisgsa <52218712+octavialarentisgsa@users.noreply.github.com>
2021-01-28 15:07:01 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,11 +3,6 @@ variable "aws_key_name" {
   description = "aws ec2 key name used to authenticate"
 }
 
-variable "instance_type" {
-  description = "EC2 instance type"
-  type = string
-}
-
 variable "subnets_private_id" {
   type    = list(string)
   description = "List of Subnet ID for EC2 instances - must cross 2 AZs due to EKS requirements"
@@ -16,14 +11,4 @@ variable "subnets_private_id" {
 variable "vpc_id" {
   type = string
   description = "ID of the VPC "
-}
-
-variable "cidr_block_worker_group_1" {
-  type    = list(string)
-  description = "List of CIDR blocks needing access to Worker Group 1"
-}
-
-variable "cidr_block_all_workers" {
-  type    = list(string)
-  description = "List of CIDR blocks needing access to all Worker Groups"
 }
