5aaa364ceb1e65509fd156538589d03ad40f3900
octavialarentisgsa <52218712+octavialarentisgsa@users.noreply.github.com>
2021-02-01 14:34:20 -0500

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,35 +1,20 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 3.20.0"
-    }
-
-    random = {
-      source  = "hashicorp/random"
-      version = "3.0.0"
-    }
-
-    local = {
-      source  = "hashicorp/local"
-      version = "2.0.0"
-    }
-
-    null = {
-      source  = "hashicorp/null"
-      version = "3.0.0"
-    }
-
-    template = {
-      source  = "hashicorp/template"
-      version = "2.2.0"
-    }
-
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.0.1"
-    }
-  }
-
-  required_version = "~> 0.14"
+  required_version = ">= 0.12"
 }
+
+provider "random" {
+  version = "~> 2.1"
+}
+
+provider "local" {
+  version = "~> 1.2"
+}
+
+provider "null" {
+  version = "~> 2.1"
+}
+
+provider "template" {
+  version = "~> 2.1"
+}
+
