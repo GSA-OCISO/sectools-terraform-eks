0106455fc31c73e2ef3460f0bca3a5e13ceb9b4b
octavialarentisgsa <52218712+octavialarentisgsa@users.noreply.github.com>
2021-02-03 18:22:00 -0500

    Update security-groups.tf

--- security-groups.tf
+++ security-groups.tf
@@ -27,6 +27,35 @@ resource "aws_security_group" "worker_group_mgmt_one" {
 
 }
 
+resource "aws_security_group" "worker_group_mgmt_two" {
+  name_prefix = "worker_group_mgmt_two"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = var.cidr_block_worker_group_2
+  }
+  
+  ingress {
+    from_port = 3000
+    to_port   = 3000
+    protocol  = "tcp"
+
+    cidr_blocks = var.cidr_block_worker_group_2
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
 resource "aws_security_group" "all_worker_mgmt" {
   name_prefix = "all_worker_management"
   vpc_id      = var.vpc_id
