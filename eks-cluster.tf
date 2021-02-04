119228c07855bdbc3767b740bb16506ae0966847
octavialarentisgsa <52218712+octavialarentisgsa@users.noreply.github.com>
2021-02-03 19:42:58 -0500

    Update eks-cluster.tf

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -17,7 +17,7 @@ module "eks" {
       name                          = "sectools-k8s-worker-group-1"
       instance_type                 = var.instance_type
       additional_userdata           = "GSA OCISO SecTools k8s cluster"
-      asg_desired_capacity          = 3
+      asg_desired_capacity          = 2
       key_name                      = var.aws_key_name
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
     },
